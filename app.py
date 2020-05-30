456e88eaf024193e64f0aa38b56a71a123a01d48
Carlos Borges <cjvborges@gmail.com>
2020-05-30 16:19:29 +0100

    melhorias no erro e inicio do login

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+global NAME
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        NAME = request.form['name']
+        return render_template("login.html", name=NAME)
+    else:
+        return render_template("login.html")
 
 @app.errorhandler(404)
 def erro(error):
@@ -8,47 +18,48 @@ def erro(error):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', name = "")
 
 @app.route('/inicio')
 def p1():
-    return render_template('inicio.html')
+    return render_template('inicio.html', name = "")
     
 @app.route('/695')
 def p2():
-    return render_template('2.html')
+    return render_template('2.html', name = "") 
+    
     
 @app.route('/935')
 def p3():
-    return render_template('3.html')
+    return render_template('3.html', name = "")
 
 @app.route('/120')
 def p4():
-    return render_template('4.html')
+    return render_template('4.html', name = "")
 
 @app.route('/963')
 def p5():
-    return render_template('5.html')
+    return render_template('5.html', name = "")
 
 @app.route('/sopas')
 def p6():
-    return render_template('6.html')
+    return render_template('6.html', name = "")
 
 @app.route('/145')
 def p7():
-    return render_template('7.html')
+    return render_template('7.html', name = "")
 
 @app.route('/varvbla')
 def p8():
-    return render_template('8.html')
+    return render_template('8.html', name = "")
     
 @app.route('/8650')
 def p9():
-    return render_template('9.html')
+    return render_template('9.html', name = "")
 
 @app.route('/345')
 def final():
-    return render_template('final.html')
+    return render_template('final.html', name = "")
 
 
 
